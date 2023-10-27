bf665f32083596744f1c35b12043c27204eb3f94
amatt06 <S3853376@student.rmit.edu.au>
2023-10-27 22:04:26 +1000

    login visible

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 from flask import Flask, render_template
 from utilities.setup import setup_data
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder="app/templates")
+
+
+@app.route('/')
+def login():
+    return render_template('login.html')
+
 
 if __name__ == '__main__':
     if setup_data():
