541c764eebd3e35abd925454d75b85c6a76d4bc7
amatt06 <S3853376@student.rmit.edu.au>
2023-10-30 01:57:22 +1000

    s3 image implementation

--- app/controller/query_handler.py
+++ app/controller/query_handler.py
@@ -1,6 +1,20 @@
 import boto3
 from flask import request, redirect, url_for, session
 
+s3 = boto3.client('s3')
+
+
+def get_image_url(artist_name):
+    try:
+        response = s3.generate_presigned_url('get_object',
+                                             Params={'Bucket': 'artist-images-bucket',
+                                                     'Key': f'artist_images/{artist_name}.jpg'},
+                                             ExpiresIn=3600)
+        return response
+    except Exception as e:
+        print(f"Error generating presigned URL: {e}")
+        return None
+
 
 def handle_query():
     title = request.form.get('title')
@@ -50,7 +64,9 @@ def handle_query():
             return redirect(url_for('main', message='No result is retrieved. Please query again'))
 
         for item in items:
-            item['img_url'] = item.get('img_url', {}).get('S', '')
+            artist_name = item['artist']['S']
+            img_url = get_image_url(artist_name)
+            item['img_url'] = img_url
 
         print(items)
         session['query_results'] = items
