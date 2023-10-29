b7a9f9aaf593fc638e63965e74c4b7289a4cc810
amatt06 <S3853376@student.rmit.edu.au>
2023-10-29 12:29:43 +1000

    clean comments

--- app/controller/login_handler.py
+++ app/controller/login_handler.py
@@ -7,15 +7,12 @@ def handle_login():
     email = request.form.get('email')
     password = request.form.get('password')
 
-    # Construct the KeyConditionExpression as a string
     key_condition_expression = "email = :email"
 
-    # Define the expression attribute values
     expression_attribute_values = {
         ":email": {"S": email}
     }
 
-    # Query DynamoDB to check if the user exists with the provided email
     response = dynamodb.query(
         TableName='login',
         KeyConditionExpression=key_condition_expression,
@@ -25,9 +22,7 @@ def handle_login():
     if response.get('Count', 0) == 1:
         stored_password = response['Items'][0]['password']['S']
 
-        # Check if the stored password matches the provided password
         if password == stored_password:
-            # Password matches, redirect to the 'main' page
             return redirect(url_for('main'))
 
     error_message = 'Invalid Email or Password.'
--- app/controller/registration_handler.py
+++ app/controller/registration_handler.py
@@ -7,18 +7,14 @@ def handle_registration():
         username = request.form.get('username')
         password = request.form.get('password')
 
-        # Check if the email already exists in the database
         user = User.query.filter_by(email=email).first()
         if user:
             return render_template('register.html', message='The email already exists.')
 
-        # If the email is unique, create a new user and add them to the database
         new_user = User(email=email, username=username, password=password)
         db.session.add(new_user)
         db.session.commit()
 
-        # Redirect the user to the login page
         return redirect(url_for('login'))
 
-    # If the request method is not POST, render the registration page
     return render_template('register.html')
